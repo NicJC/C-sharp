0e5fc7fc22a3f446350b73b295a7b339ac143d04
Nicholas Coxen <nicjcoxen@gmail.com>
2021-12-31 08:54:24 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" C# Variables (various approaches):");
+var test = "Hello";
+string tst;
+tst = "test";
+int number = 15;
+string name;
+name = "Nic";
+double dnumber = 0.09d;
+decimal decs = 1.15m;
+Console.WriteLine(test + "," + "my name is " + name + "," + "this is a " + tst + " plus an integer " + number + "," + " and " + "a double " + dnumber + " and a decimal number " + decs);
+
